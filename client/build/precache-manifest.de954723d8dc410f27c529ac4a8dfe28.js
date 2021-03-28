72d0dd3bf09e2c42c8fa644f57e79fe83414ca95
CassBut <clhurlbut@gmail.com>
2021-03-27 20:55:36 -0700

    added node modules

---
+++ client/build/precache-manifest.de954723d8dc410f27c529ac4a8dfe28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e20e5a74db0fe2a2b6577a8e2143e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef4bf53aa80ee72b118",
+    "url": "/static/css/main.88131917.chunk.css"
+  },
+  {
+    "revision": "290db1e9b5351c3a8ccb",
+    "url": "/static/js/2.1cb0ce72.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.1cb0ce72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef4bf53aa80ee72b118",
+    "url": "/static/js/main.8ca28571.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);

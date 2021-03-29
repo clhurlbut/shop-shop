04db5530b8c2e0c23e7c90de3756cb4b70f3cb61
CassBut <clhurlbut@gmail.com>
2021-03-28 21:34:10 -0700

    new build

---
+++ client/build/precache-manifest.76e5792b1679d2c401c72b261c49a43c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e2393a265bc23c292602094c29737b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07464b9c96d49950eece",
+    "url": "/static/css/main.88131917.chunk.css"
+  },
+  {
+    "revision": "4a06c54aaed5317a78ba",
+    "url": "/static/js/2.de721efe.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.de721efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07464b9c96d49950eece",
+    "url": "/static/js/main.c5e3d074.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);

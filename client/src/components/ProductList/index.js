882a65fb14ab32dab8af5d1ab3a77374da1ffc4a
CassBut <clhurlbut@gmail.com>
2021-03-28 21:33:00 -0700

    trying to fix state error in product list

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,52 +1,56 @@
 import React, { useEffect } from "react";
-import ProductItem from "../ProductItem";
-/* import { useStoreContext } from "../../utils/GlobalState"; */
-import { useDispatch, useSelector } from "react-redux";
-import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { useQuery } from '@apollo/react-hooks';
+import ProductItem from "../ProductItem";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { QUERY_PRODUCTS } from "../../utils/queries";
-import { idbPromise } from "../../utils/helpers";
+import { idbPromise } from '../../utils/helpers';
 import spinner from "../../assets/spinner.gif"
+import { useDispatch, useSelector } from "react-redux";
 
 function ProductList() {
-  // const [state, dispatch] = useStoreContext();
   const dispatch = useDispatch();
-
   const { currentCategory, products } = useSelector(state => ({currentCategory:state.currentCategory, products:state.products}));
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    if(data) {
+    // if there's data to be stored
+    if (data) {
+      // let's store it in the global state object
       dispatch({
-           type: UPDATE_PRODUCTS,
-          products: data.products
-        });
-        data.products.forEach((product) => {
-          idbPromise('products', 'put', product);
-        });
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+  
+      // but let's also take each product and save it to IndexedDB using the helper function 
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
     } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
       idbPromise('products', 'get').then((products) => {
         dispatch({
           type: UPDATE_PRODUCTS,
-         products: products
-       });
+          products: products
+        });
       });
     }
   }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return state.products;
+      return products;
     }
 
-    return state.products.filter(product => product.category._id === currentCategory);
+    return products.filter(product => product.category._id === currentCategory);
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {products.length ? (
         <div className="flex-row">
             {filterProducts().map(product => (
                 <ProductItem
